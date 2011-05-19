13e2e4a4825e8982fc64e0bfbd3aa26056c60932
Dan Melton <melton.dan@gmail.com>
2011-05-19 12:27:21 -0700

    Bumped version

--- lib/recalls/client/products.rb
+++ lib/recalls/client/products.rb
@@ -4,10 +4,29 @@ module Recalls
   class Client
     module Products
     include Recalls::Client::Connection
+    
+    # Returns a hash of products matching the search.
+    #
+    # @return {Hash}
+    # @param query
+    # @param start_date
+    # @param end_date
+    # @param organization opptions NHSTA, CPSC, CDC
+    # @param upc
+    # @param sort sort=date 
+    # @param page
+    # @param year only for NHSTA (Traffic)
+    # @param make only for NHSTA (Traffic)
+    # @param model only for NHSTA (Traffic)        
+    # @see http://search.usa.gov/api/recalls
+    # @example Returns results based upon queries
+    #   Recalls.search(:query => "bad cheese")
+    
       def search(options = {})
         options.merge!({:format => "json"})                
-        response = get('', options) rescue "Invalid API Key"
+        response = get('', options) rescue "Invalid API Key"  
       end
+      
     end
   end
 end
--- lib/recalls/version.rb
+++ lib/recalls/version.rb
@@ -1,3 +1,3 @@
 module Recalls
-  VERSION = "0.0"
+  VERSION = "0.1"
 end
